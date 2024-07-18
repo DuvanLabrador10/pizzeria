7e0b45bb6141fa88b5267596ab2df6d565ca5ecd
duvanlabrador <carlos.labrador@bebolder.co>
2024-07-18 16:38:03 -0500

    Add doc

---
+++ src/main/java/com/duvanlabrador/Pizzas/infrastructure/adapters/documentation/OpenApiConfig.java
@@ -0,0 +1,36 @@
+package com.duvanlabrador.Pizzas.infrastructure.adapters.documentation;
+
+import com.duvanlabrador.Pizzas.infrastructure.adapters.util.Constants;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenApiConfig() {
+
+        Contact contact = new Contact();
+        contact.setEmail(Constants.SWAGGER_CONTACT_EMAIL_MESSAGE);
+        contact.setName(Constants.SWAGGER_CONTACT_NAME_MESSAGE);
+        contact.setUrl(Constants.SWAGGER_CONTACT_URL_MESSAGE);
+
+        License license = new License();
+        license.setName(Constants.SWAGGER_LICENSE_NAME_MESSAGE);
+        license.setUrl(Constants.SWAGGER_LICENSE_URL_MESSAGE);
+
+        Info info = new Info()
+                .title(Constants.SWAGGER_TITLE_MESSAGE)
+                .description(Constants.SWAGGER_DESCRIPTION_MESSAGE)
+                .version(Constants.SWAGGER_VERSION_MESSAGE)
+                .contact(contact)
+                .license(license)
+                .termsOfService(Constants.SWAGGER_TERMS_OF_SERVICE_MESSAGE);
+
+        return new OpenAPI().info(info);
+    }
+}
+
---
+++ src/main/java/com/duvanlabrador/Pizzas/infrastructure/adapters/util/Constants.java
@@ -0,0 +1,16 @@
+package com.duvanlabrador.Pizzas.infrastructure.adapters.util;
+
+public class Constants {
+    private Constants() {
+        throw new IllegalStateException("Utility class");
+    }
+    public static final String SWAGGER_TITLE_MESSAGE = "Request Student API";
+    public static final String SWAGGER_DESCRIPTION_MESSAGE = "This is a API for students";
+    public static final String SWAGGER_VERSION_MESSAGE = "1.0";
+    public static final String SWAGGER_CONTACT_NAME_MESSAGE = "CARLOS DUVAN LABRADOR HERNANDEZ";
+    public static final String SWAGGER_CONTACT_EMAIL_MESSAGE = "carlosduvanlh@gmail.com";
+    public static final String SWAGGER_CONTACT_URL_MESSAGE = "https://github.com/DuvanLabrador10/student/tree/main/src/main/java/com/duvanlabrador/Pizzas";
+    public static final String SWAGGER_LICENSE_NAME_MESSAGE = "Apache 2.0";
+    public static final String SWAGGER_LICENSE_URL_MESSAGE = "http://www.apache.org/licenses/LICENSE-2.0";
+    public static final String SWAGGER_TERMS_OF_SERVICE_MESSAGE = "Terms of service";
+}
